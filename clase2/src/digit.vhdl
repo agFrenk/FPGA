bd5a67badd3e1aae483b3b313c3d1237a9d01524
sadskinhead <afus.frenkel@gmail.com>
2023-04-20 23:33:43 -0300

    comenzamos el ejercicio del contador. falta testear la integracion

---
+++ clase2/src/digit.vhdl
@@ -0,0 +1,41 @@
+library ieee;
+library utils;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity digit is
+    generic(
+        N:natural := 4
+    );
+    port(
+        clk_i           : in std_logic;
+        reset_i          : in std_logic;
+        carry_o         : out std_logic;
+        clr_o           : out std_logic_vector(N-1 downto 0) 
+    );
+end entity;
+
+architecture digit_architecture of digit is
+    signal carry        : std_logic := '0';
+    signal acumulator   : unsigned(N-1 downto 0) := to_unsigned(0, N);
+begin
+    process(clk_i, reset_i) -- falta logica de que se active en el rising edge
+    begin
+        if (clk_i'event and clk_i = '1') then
+            if(reset_i = '1') then
+                acumulator <= (others => '0');
+            else
+                if(acumulator = to_unsigned(9, N)) then
+                    acumulator <= (others => '0');
+                    carry <= '0';
+                else
+                    acumulator <= acumulator + 1;
+                    carry <= '1';
+                end if;
+            end if;
+        end if;
+    end process;
+    
+    carry_o <= carry;
+    clr_o <= std_logic_vector(acumulator);
+end architecture;
---
+++ clase2/testbench/digit_tb.vhd
@@ -0,0 +1,54 @@
+library IEEE;
+library utils;
+use IEEE.std_logic_1164.all;
+
+entity digit_tb is
+end entity;
+
+architecture tb_arch of digit_tb is
+  constant N : natural := 4;
+    -- import the entity to be tested
+    component digit is
+        generic(N:natural);
+        port(
+            clk_i           : in std_logic;
+            reset_i          : in std_logic;
+            carry_o         : out std_logic;
+            clr_o           : out std_logic_vector(N-1 downto 0) 
+        );
+    end component;
+    
+    -- signals to connect to the entity under test
+
+    --Inputs
+    signal clk     : std_logic := '0';
+    signal reset_i :  std_logic := '0';
+    --Outputs
+    signal carry_o: std_logic;
+    signal clr_o : std_logic_vector(N-1 downto 0) := (others => '0');
+
+  -- Clock period definitions
+  constant clk_period : time := 1 ms;
+
+begin
+    -- instantiate the entity under test
+    uut: digit
+    generic map(N => 4)
+    port map(
+        clk_i => clk,
+        reset_i => reset_i,
+        carry_o => carry_o,
+        clr_o => clr_o
+
+    );
+
+    -- Clock process definitions
+    clk_process : process
+    begin
+      clk <= '1';
+      wait for clk_period/2;
+      clk <= '0';
+      wait for clk_period/2;
+    end process;
+        
+end architecture;
